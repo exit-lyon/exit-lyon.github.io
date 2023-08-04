ca9095d70811d6abed0b40f5fc1d9f7bac6d0841
vql <valentinjossic@outlook.fr>
2023-08-04 21:39:47 +0200

    Better carousel and welcome header

--- index.js
+++ index.js
@@ -18,7 +18,12 @@ const carouselCards = $('.carousel-card')
 const carouselLeftButton = $('#left-carousel-button')
 const carouselRightButton = $('#right-carousel-button')
 
+let blockCarouselAutoPlay = false
+let carouselAutoUpdateDelay = 5000
+
 updateCarousel()
+
+var carouselInterval = setInterval(carouselAutoPlay, carouselAutoUpdateDelay);
 
 carouselLeftButton.click(() => {
     carouselCards.each(function () {
@@ -39,27 +44,58 @@ carouselRightButton.click(() => {
 })
 
 $(document).on("click", '.carousel-card[data-status=side-left]', function () {
-    if ($(window).width() > 800) return
-    console.log('clicked left')
     carouselCards.each(function () {
         let dataIndex = parseInt($(this).attr('data-index'))
         dataIndex = (dataIndex + 1) % carouselCards.length
         $(this).attr('data-index', dataIndex)
     })
+    $(this).attr('data-center', 'left')
+    setTimeout(() => {
+        $(this).attr('data-center', null)
+    }, 750);
     updateCarousel()
+    clearCarouselAutoPlay()
 })
 
 $(document).on("click", ".carousel-card[data-status=side-right]", function () {
-    if ($(window).width() > 800) return
-    console.log('clicked right')
     carouselCards.each(function () {
+        let dataIndex = parseInt($(this).attr('data-index'))
+        dataIndex = dataIndex - 1 < 0 ? carouselCards.length - 1 : dataIndex - 1
+        $(this).attr('data-index', dataIndex)
+    })
+    $(this).attr('data-center', 'right')
+    setTimeout(() => {
+        $(this).attr('data-center', null)
+    }, 750);
+    updateCarousel()
+    clearCarouselAutoPlay()
+})
+
+function carouselAutoPlay() {
+    carouselCards.each(function () {
+        if ($(this).attr("data-status") == 'side-right') {
+            $(this).attr('data-center', 'right')
+            setTimeout(() => {
+                $(this).attr('data-center', null)
+            }, 750);
+        }
 
         let dataIndex = parseInt($(this).attr('data-index'))
         dataIndex = dataIndex - 1 < 0 ? carouselCards.length - 1 : dataIndex - 1
         $(this).attr('data-index', dataIndex)
     })
     updateCarousel()
-})
+}
+
+var timeout
+
+function clearCarouselAutoPlay() {
+    clearInterval(carouselInterval)
+    clearTimeout(timeout)
+    timeout = setTimeout(() => {
+        carouselInterval = setInterval(carouselAutoPlay, carouselAutoUpdateDelay)
+    }, 10000);
+}
 
 
 function updateCarousel() {
